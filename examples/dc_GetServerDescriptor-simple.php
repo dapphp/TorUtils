9ee940faa70b12c8c0d36b2e220d9be5b4a044b1
dapphp <drew@drew-phillips.com>
2017-06-04 13:48:49 -0700

    Handle exception; show setPreferredServer()

--- examples/dc_GetServerDescriptor-simple.php
+++ examples/dc_GetServerDescriptor-simple.php
@@ -5,8 +5,14 @@ require_once 'common.php';
 use Dapphp\TorUtils\DirectoryClient;
 
 $client = new DirectoryClient();
+//$client->setPreferredServer('1.2.3.4:80'); // Optional server to always use first for directory lookups
 
-$descriptor = $client->getServerDescriptor('79E169B25E4C7CE99584F6ED06F379478F23E2B8');
+try {
+    $descriptor = $client->getServerDescriptor('79E169B25E4C7CE99584F6ED06F379478F23E2B8');
+} catch (\Exception $ex) {
+    echo "Request to directory failed: " . $ex->getMessage() . "\n";
+    exit;
+}
 
 echo sprintf("%-19s %40s\n", $descriptor->nickname, $descriptor->fingerprint);
 echo sprintf("Running %s\n", $descriptor->platform);
